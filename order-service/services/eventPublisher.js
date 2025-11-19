700c7ee625f502191f198b120965638bc404b7a6
Nguyen Tuong Vinh <tuongv03v@gmail.com>
2025-11-20 06:56:03 +0700

    Add log to see whether SERVICE_BUS_CONNECTION_STRING was imported or not

--- order-service/services/eventPublisher.js
+++ order-service/services/eventPublisher.js
@@ -4,6 +4,9 @@ const { ServiceBusClient } = require("@azure/service-bus");
 require("dotenv").config();
 // Biến môi trường này cần được thiết lập trong Azure App Service Configuration
 const CONNECTION_STRING = process.env.SERVICEBUS_CONNECTION_STRING;
+console.log(
+  "SERVICEBUS_CONNECTION_STRING: " + process.env.SERVICEBUS_CONNECTION_STRING
+);
 const TOPIC_NAME = "order-created-topic";
 
 const sbClient = new ServiceBusClient(CONNECTION_STRING);
